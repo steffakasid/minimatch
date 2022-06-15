a3930a9e0560ac1888359b4e9868000e9fca8c79
steffakasid <github@steffen-rumpf.de>
2022-06-15 10:23:15 +0200

    doc: Add Readme.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# minimatch
+
+This is just a simple project to test out glob patterns used in Renovate (renovate uses minimatch to resolve glob patterns).
+
+You can find more details about globbing syntax in: https://www.gnu.org/software/bash/manual/bash.html#Pattern-Matching
--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const minimatch = require("minimatch")
 
 const fileList = [
-    "dbs-businesshub/bizhub-apionly/someapi",
-    "dbs-businesshub/bizhub-tools/someproject",
-    "dbs-businesshub/bizhub-connect/kong-shared",
-    "dbs-businesshub/bizhub-apic/topology",
-    "dbs-businesshub/bizhub-tools/bizhub-helm/helmchart",
-    "dbs-businesshub/ibm-api-connect/ibm-api-connect",
-    "dbs-businesshub/smartlocker/ibm-api-connect"
+    "top-level1/sub-group-level1-prefix-apionly/someapi",
+    "top-level1/sub-group-level1-prefix-tools/someproject",
+    "top-level1/sub-group-level1-prefix-connect/kong",
+    "top-level1/sub-group-level1-prefix-apic/topology",
+    "top-level1/sub-group-level1-prefix-tools/sub-group-level2-helm/helmchart",
+    "top-level1/ibm-api-connect/ibm-api-connect",
+    "top-level1/smartlocker/ibm-api-connect",
+    "top-level2/sub-group-level1-prefix-connect/kong-shared",
+    "top-level2/sub-group-level1-prefix-apic/something",
+    "top-level2/ibm-api-connect/ibm-api-connect"
 ]
 
-console.log(minimatch.match(fileList, "dbs-businesshub/{bizhub!(-apionly),ibm-api-connect}/**/*", {matchBase: true}))
+console.log(minimatch.match(fileList, "?(top-level2|top-level1)/{sub-group-level1-prefix!(-api-only|-playground),ibm-api-connect}/**/*", {matchBase: true}))
